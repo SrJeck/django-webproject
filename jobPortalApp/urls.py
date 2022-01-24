f40ac98d86babc35d1f70fb23a7aaeb8fb9b184d
Ogie11 <benitez.alfredo.b.1128@gmail.com>
2022-01-24 23:54:20 +0800

    fix some overlap

--- jobPortalApp/urls.py
+++ jobPortalApp/urls.py
@@ -11,3 +11,42 @@ urlpatterns = [
 urlpatterns += [
     path('seeker-info/', views.seeker_no_info, name="seeker_no_info"),
 ]
+
+
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.home, name="Home"),
+    path('creatt/', views.index),
+]
+
+# benitez
+# for job seeker profile
+urlpatterns += [
+    path('seeker-info/', views.seeker_no_info, name="seeker_no_info"),
+]
+
+
+# mohammed
+# administrator
+urlpatterns += [
+    path('admin_dashboard/', views.admin_dashboard, name="admin_dashboard"),
+]
+
+
+# lowell
+# for Company/Job Provider
+urlpatterns += {
+    path('provider-no-info/', views.provider_no_info, name="provider_no_info"),
+    path('provider-with-info/', views.provider_with_info, name="provider_with_info"),
+
+    path('provider-edit-info/', views.provider_edit_info, name="provider_edit_info"),
+
+    path('provider-show-job-post/', views.provider_show_job_post, name="provider_show_job_post"),
+    path('provider-edit-job-post/', views.provider_edit_job, name="provider_edit_job_post"),
+
+    path('provider-show-applicant/', views.provider_show_applicant, name="provider_show_applicant"),
+
+}
+
--- jobPortalApp/views.py
+++ jobPortalApp/views.py
@@ -12,6 +12,41 @@ def home(request):
     return render(request, "jobPortalApp/index.html")
 
 
+def admin_dashboard(request):
+    return render(request, "jobPortalApp/admin/admin_dashboard.html")
+
+
 # job seeker
 def seeker_no_info(request):
     return render(request, "jobPortalApp/pages/profile/seeker/no-info.html")
+
+
+def seeker_no_info(request):
+    state = request.session['active'] = True
+    return render(request, "jobPortalApp/pages/profile/seeker/no-info.html", {"state": state})
+
+
+# job provider
+def provider_no_info(request):
+    return render(request, "jobPortalApp/pages/profile/provider/no-info.html")
+
+
+def provider_with_info(request):
+    return render(request, "jobPortalApp/pages/profile/provider/with-info.html")
+
+
+def provider_edit_info(request):
+    return render(request, "jobPortalApp/pages/profile/provider/add-edit-info.html")
+
+
+def provider_show_job_post(request):
+    return render(request, "jobPortalApp/pages/profile/provider/show-job-post.html")
+
+
+def provider_edit_job(request):
+    return render(request, "jobPortalApp/pages/profile/provider/add-edit-job.html")
+
+
+def provider_show_applicant(request):
+    return render(request, "jobPortalApp/pages/profile/provider/show-applicant.html")
+
